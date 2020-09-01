e5ba447e504ca58b3a94bf7e6e12a8ca648a2121
Iulian Popa <bobel2007@outlook.com>
2020-09-01 15:44:47 +0300

    Add obstacles

--- game.js
+++ game.js
@@ -2,8 +2,10 @@ let birdBottom = 370;
 const gravityFactor = 2;
 
 let bird = document.querySelector(".bird");
+let container = document.querySelector('.game_container');
 const background = document.querySelector(".background");
 const ground = document.querySelector(".ground");
+// bird.classList.add('animate');
 
 // document.addEventListener('DOMContentLoaded', initGame);
 initGame();
@@ -13,21 +15,64 @@ function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
 
     gravity();
-    document.addEventListener('keyup', jump, bird);
+    document.addEventListener('keyup', jump);
+    let obstacleInterval = setInterval(genObstacle, 3000);
+    genObstacle();
+
+    // gameOver();
 }
 
 
 function gravity() {
-    // let bottom = birdBottom;
 
-    setInterval(() => {
+    let gravityInterval = setInterval(() => {
         birdBottom -= gravityFactor;
-        // console.log('am trecut');
         bird.style.bottom = `${birdBottom}px`;
-        // console.log(element.style.bottom);
+        gameOver(gravityInterval);
     }, 20);
-} 
+    return gravityInterval;
+}
+
 
 function jump() {
     birdBottom += 70;
+    bird.classList.remove('animate');
+    setTimeout(() => bird.classList.add('animate'), 20);
+}
+
+
+function gameOver (interval) {
+    if (birdBottom < 150) {
+        console.log('game over');
+        clearInterval(interval);
+    }
+}
+
+
+function genObstacle () {
+    let obstacleLeft = 500;
+    let obstacleBottom = 150;
+    let obstacleTop
+    let obstacle = document.createElement('div');
+    obstacle.classList.add('obstacle');
+    container.appendChild(obstacle);
+    obstacle.style.left = `${obstacleLeft}px`;
+    obstacle.style.bottom = `${obstacleBottom}px`;
+
+    let topObstacle = document.createElement('div');
+    topObstacle.classList.add('obstacle');
+    container.appendChild(topObstacle);
+    topObstacle.style.left = `${obstacleLeft}px`;
+    topObstacle.style.bottom = `${obstacleBottom + 430}px`;
+
+    setInterval(() => {
+        obstacleLeft -= 2
+        obstacle.style.left = `${obstacleLeft}px`;
+        topObstacle.style.left = `${obstacleLeft}px`;}, 20)
+
+
+}
+
+function moveObstacle () {
+
 }
