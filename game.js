f2063462a200ff4cc1f6f96bfb2af72717efb4bd
Iulian Popa <bobel2007@outlook.com>
2020-09-01 11:19:50 +0300

    Add bird jump and gravity

--- game.js
+++ game.js
@@ -1,7 +1,33 @@
+let birdBottom = 370;
+const gravityFactor = 2;
+
+let bird = document.querySelector(".bird");
+const background = document.querySelector(".background");
+const ground = document.querySelector(".ground");
+
+// document.addEventListener('DOMContentLoaded', initGame);
 initGame();
 
 function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
 
+    gravity();
+    document.addEventListener('keyup', jump, bird);
+}
+
+
+function gravity() {
+    // let bottom = birdBottom;
+
+    setInterval(() => {
+        birdBottom -= gravityFactor;
+        // console.log('am trecut');
+        bird.style.bottom = `${birdBottom}px`;
+        // console.log(element.style.bottom);
+    }, 20);
+} 
+
+function jump() {
+    birdBottom += 70;
 }
